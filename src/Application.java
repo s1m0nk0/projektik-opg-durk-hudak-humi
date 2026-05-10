d97b0efe17aef022bdebe9df71287906278b8ec4
simondurkac <simon.presov@gmail.com>
2026-05-10 11:48:56 +0200

    scenebuilder vybavenko, chyba stylovanie popripade rozlozenie do viacerych okien

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,17 @@
-public class Application {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+        Scene scene = new Scene(root, 900, 600);
+
+        stage.setTitle("TODO aplikacia");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
